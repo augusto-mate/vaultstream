5affac10ffbdd2263a989ff5f59275865b5e6f12
Augusto Mate <mate.augusto.mz@gmail.com>
2026-01-17 21:12:34 +0200

    refactor: refactor streamlit_app.py by removing unnecessary imports

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,12 @@
 # 👑 streamlit_app.py
 
-import sys
-from pathlib import Path
-
-ROOT_DIR = Path(__file__).resolve().parent
-sys.path.append(str(ROOT_DIR))
-
 import streamlit as st
 from core.pipeline import run_pipeline
-
-st.markdown("""
-    <style>
-    .stProgress > div > div > div > div { background-color: #F9AB00; }
-    .stButton>button { background-color: #F9AB00; color: white; border-radius: 8px; }
-    </style>
-    """, unsafe_allow_html=True)
 
 st.set_page_config(page_title="VaultStream", layout="centered")
 
 st.title("🚀 VaultStream")
-st.write("Torrent → Criptografia → Nuvem → Limpeza")
+st.write("Torrent  →  Criptografia  →  Nuvem  →  Limpeza")
 
 magnet = st.text_area("Cole o magnet link")
 
